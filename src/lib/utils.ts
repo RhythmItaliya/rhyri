f74c979b8955d53a985898142230cbca4ae2d5f9
rhythmitaliya <ritaliya2003@gmail.com>
2024-08-21 22:54:35 +0530

    21-08-2024

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -32,8 +32,8 @@ export function formatFirestoreTimestamp(timestamp: Timestamp) {
 }
 
 export function formatCurrency(amount: number) {
-  return new Intl.NumberFormat("en-US", {
+  return new Intl.NumberFormat("en-IN", {
     style: "currency",
-    currency: "USD",
+    currency: "INR",
   }).format(amount)
 }
